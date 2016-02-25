4f4da031f82328dc425d71927816dd96f2f48dbe
Amit Dubey <amit.dubey2219@gmail.com>
2016-02-24 22:06:44 -0800

    added sample rest api code and removed spring secutiy for now

---
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/controller/SamleController.java
@@ -0,0 +1,13 @@
+package com.bluewall.feservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SamleController {
+
+    @RequestMapping("/hello")
+    public String helloRestApi() {
+        return "Hello world";
+    }
+}
