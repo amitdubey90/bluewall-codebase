41bb4c2cf283004b15582b99491ba6d51135f7c9
Doshi <vjdoshi@informatica.com>
2016-03-12 00:17:09 -0800

    adding food log service

--- bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/service/FoodService.java
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/service/FoodService.java
@@ -6,6 +6,10 @@ import com.bluewall.util.bean.UserFoodLog;
 
 public interface FoodService {
 	
+	/*
+	 * returns list of objects for user food logs for a given user id.
+	 */
+	
 	public List<UserFoodLog> getUserFoodLog(int userID);
 	
 }
