dd46c10e7aa68cbbd20fad3082c22e7717343822
jenil-doshi <jenil.doshi@sjsu.edu>
2016-03-11 17:27:58 -0800

    Fix for Deprecated Date for Calorie api, Commenting exclusion for log in pom.

--- bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/daoImpl/CalorieDaoImpl.java
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/daoImpl/CalorieDaoImpl.java
@@ -3,8 +3,9 @@ package com.bluewall.feservices.daoImpl;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
 import javax.sql.DataSource;
 
@@ -27,25 +28,28 @@ public class CalorieDaoImpl implements CalorieDao {
 	@Autowired
 	DataSource dataSource;
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public int getSumCaloriesBurnt(int userID) {
 
 		int totalCaloriesBurnt = 0;
 		log.info("Fetching Sum of Caloreies Burnt for a day");
-
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
-		Date date = new Date();
-		String currentDateTime = dateFormat.format(date);
-		date.setHours(00);
-		date.setMinutes(00);
-		date.setSeconds(00);
-		String startDateTime = dateFormat.format(date);
-
-		try (ResultSet resultSet = dataSource
-				.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_BURNT + " where userID = " + userID
-						+ " and startTime >= '" + startDateTime + "' and startTime <= '" + currentDateTime + "'")
-				.executeQuery()) {
+		
+		Calendar calendar = Calendar.getInstance();
+		String currentDateTime = dateFormat.format(calendar.getTime());
+		try {
+			calendar.setTime(dateFormat.parse(currentDateTime));
+		} catch (ParseException e1) {
+			log.error("Exception occured while parsing Date",e1);
+		}
+		calendar.set(Calendar.HOUR_OF_DAY, 00);
+		calendar.set(Calendar.MINUTE, 00);
+		calendar.set(Calendar.SECOND, 00);
+		calendar.set(Calendar.MILLISECOND, 00);
+		String startDateTime = dateFormat.format(calendar.getTime());
+		try (ResultSet resultSet = dataSource.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_BURNT + " where userID = " + userID
+						+ " and startTime >= '" + startDateTime + "' and startTime <= '" + currentDateTime + "'").executeQuery()) {
+			log.info("Data fetched from database successfully");
 			if (resultSet.next())
 				totalCaloriesBurnt = resultSet.getInt("caloriesBurnt");
 		} catch (SQLException e) {
@@ -55,27 +59,28 @@ public class CalorieDaoImpl implements CalorieDao {
 		return totalCaloriesBurnt;
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public int getSumCaloriesConsumed(int userID) {
 
 		int totalCaloriesConsumed = 0;
 
 		log.info("Fetching Sum of Caloreies Consumed for a day");
-
-		/* Calculating Start time and current time of the date */
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
-		Date date = new Date();
-		String currentDateTime = dateFormat.format(date);
-		date.setHours(00);
-		date.setMinutes(00);
-		date.setSeconds(00);
-
-		String startDateTime = dateFormat.format(date);
-		try (ResultSet resultSet = dataSource.getConnection()
-				.prepareStatement(Queries.GET_TOTAL_CALORIE_CONSUMED + " where userID = " + userID
-						+ " and foodLogTime >= '" + startDateTime + "' and foodLogTime <= '" + currentDateTime + "'")
-				.executeQuery()) {
+		Calendar calendar = Calendar.getInstance();
+		String currentDateTime = dateFormat.format(calendar.getTime());
+		try {
+			calendar.setTime(dateFormat.parse(currentDateTime));
+		} catch (ParseException e1) {
+			log.error("Exception occured while parsing Date",e1);
+		}
+		calendar.set(Calendar.HOUR_OF_DAY, 00);
+		calendar.set(Calendar.MINUTE, 00);
+		calendar.set(Calendar.SECOND, 00);
+		calendar.set(Calendar.MILLISECOND, 00);
+		String startDateTime = dateFormat.format(calendar.getTime());
+		try (ResultSet resultSet = dataSource.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_CONSUMED + " where userID = " + userID
+						+ " and foodLogTime >= '" + startDateTime + "' and foodLogTime <= '" + currentDateTime + "'").executeQuery()) {
+			log.info("Data fetched from database successfully");
 			if (resultSet.next())
 				totalCaloriesConsumed = resultSet.getInt("weightConsumed");
 		} catch (SQLException e) {
