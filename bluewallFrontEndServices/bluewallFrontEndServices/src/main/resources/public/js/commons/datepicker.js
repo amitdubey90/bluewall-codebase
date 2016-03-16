6a1648edd8264f23d3f2c5748716f83473a82de5
jenil-doshi <jenil.doshi@sjsu.edu>
2016-03-15 20:41:47 -0700

    Adding Datepicker in Dashboard page

---
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/resources/public/js/commons/datepicker.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+$(function() {
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+    cb(moment().subtract(29, 'days'), moment());
+
+    $('#reportrange').daterangepicker({
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+});
+</script>
