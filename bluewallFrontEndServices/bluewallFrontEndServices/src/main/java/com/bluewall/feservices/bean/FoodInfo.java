b454df974f1cb4cc9bcb5ef111202f075ae8e82c
Amit Dubey <amit.dubey2219@gmail.com>
2016-04-20 23:14:58 -0700

    Revert "removing duplicate pojo FoodInfo"
    This reverts commit 32558d25dc807d73492e0f1c2eb40f071314c89b.

---
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/bean/FoodInfo.java
@@ -0,0 +1,34 @@
+package com.bluewall.feservices.bean;
+
+public class FoodInfo {
+
+	private int foodId;
+	private String foodName;
+	private double foodCalorie;
+	
+	public FoodInfo(){
+	}
+	
+	public int getFoodId() {
+		return foodId;
+	}
+	public void setFoodId(int foodId) {
+		this.foodId = foodId;
+	}
+	public String getFoodName() {
+		return foodName;
+	}
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+	public double getFoodCalorie() {
+		return foodCalorie;
+	}
+	public void setFoodCalorie(double foodCalorie) {
+		this.foodCalorie = foodCalorie;
+	}
+	
+	
+	
+	
+}
