ce38bf05278c9e76cfa2c07dfb26b5833f730486
vrushankd <vrushank.doshi90@gmail.com>
2016-05-05 22:07:18 -0700

    closing connections to mysql.

--- bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/daoImpl/CalorieDaoImpl.java
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/daoImpl/CalorieDaoImpl.java
@@ -25,20 +25,32 @@ public class CalorieDaoImpl implements CalorieDao {
 
 	@Override
 	public int getSumCaloriesBurnt(int userID, String date) {
-
+		
+		ResultSet resultSet = null;
 		int totalCaloriesBurnt = 0;
 		log.info("Fetching Sum of Caloreies Burnt for a day");
 		String currentDateTime = date + " 23:59:59";
 		String startDateTime = date + " 00:00:00";
-		try (ResultSet resultSet = dataSource.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_BURNT + " where userID = " + userID
-						+ " and logTime >= '" + startDateTime + "' and logTime <= '" + currentDateTime + "'").executeQuery()) {
+		try 
+		{
+			resultSet = dataSource.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_BURNT + " where userID = " + userID
+					+ " and logTime >= '" + startDateTime + "' and logTime <= '" + currentDateTime + "'").executeQuery();
 			log.info("GET SUM CALORIES BURNT: Data fetched from database successfully");
 			if (resultSet.next())
 				totalCaloriesBurnt = resultSet.getInt("caloriesBurnt");
 		} catch (SQLException e) {
 			log.error("GET SUM CALORIES BURNT: SQL Exception occured while fetching user details");
 		}
+		finally{
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					log.error("GET SUM CALORIES BURNT: Result set object is not closed");
+				}
+			}
 
+		}
 		return totalCaloriesBurnt;
 	}
 
@@ -46,17 +58,27 @@ public class CalorieDaoImpl implements CalorieDao {
 	public int getSumCaloriesConsumed(int userID, String date) {
 
 		int totalCaloriesConsumed = 0;
-
+		ResultSet resultSet = null;
 		log.info("Fetching Sum of Caloreies Consumed for a day");
 		String currentDateTime = date + " 23:59:59";
 		String startDateTime = date + " 00:00:00";
-		try (ResultSet resultSet = dataSource.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_CONSUMED + " where userID = " + userID
-						+ " and logTime >= '" + startDateTime + "' and logTime <= '" + currentDateTime + "'").executeQuery()) {
+		try {
+			resultSet = dataSource.getConnection().prepareStatement(Queries.GET_TOTAL_CALORIE_CONSUMED + " where userID = " + userID
+					+ " and logTime >= '" + startDateTime + "' and logTime <= '" + currentDateTime + "'").executeQuery();
 			log.info("GET SUM CALORIES CONSUMED: Data fetched from database successfully");
 			if (resultSet.next())
 				totalCaloriesConsumed = resultSet.getInt("weightConsumed");
 		} catch (SQLException e) {
 			log.error("GET SUM CALORIES CONSUMED: SQL Exception occured while fetching user details");
+		}
+		finally{
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					log.error("GET SUM CALORIES BURNT: Result set object is not closed");
+				}
+			}
 		}
 
 		return totalCaloriesConsumed;
@@ -64,14 +86,26 @@ public class CalorieDaoImpl implements CalorieDao {
 
 	@Override
 	public int getTargetWeight(int userID) {
+		
 		int dailyCalories = 0;
+		ResultSet resultSet = null;
 		log.info("Fetching target weight for a day");
-		try (ResultSet resultSet = dataSource.getConnection().prepareStatement(Queries.GET_DAILY_CALORIES + " where userID = " + userID).executeQuery()) {
+		try {
+			resultSet = dataSource.getConnection().prepareStatement(Queries.GET_DAILY_CALORIES + " where userID = " + userID).executeQuery();
 			log.info("GET TARGET WEIGHT: Data fetched from database successfully");
 			if (resultSet.next())
 				dailyCalories = resultSet.getInt("dailyCalories");
 		} catch (SQLException e) {
 			log.error("GET TARGET WEIGHT: SQL Exception occured while fetching user details");
+		}
+		finally{
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					log.error("GET SUM CALORIES BURNT: Result set object is not closed");
+				}
+			}
 		}
 
 		return dailyCalories;
