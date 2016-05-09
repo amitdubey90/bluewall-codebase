e78af0479ed42c909cd83218b72b651e925fb512
Amit Dubey <amit.dubey2219@gmail.com>
2016-05-08 17:15:22 -0700

    factor in required calories while making recommendations

--- bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/controller/RecommendationController.java
+++ bluewallFrontEndServices/bluewallFrontEndServices/src/main/java/com/bluewall/feservices/controller/RecommendationController.java
@@ -1,9 +1,12 @@
 package com.bluewall.feservices.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import com.bluewall.feservices.service.CalorieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,8 @@ public class RecommendationController {
 	
 	@Autowired
 	FoodService foodService;
+	@Autowired
+	CalorieService calorieService;
 
 	@RequestMapping("get/{count}")
 	public List<FoodInfo> getRecommendations(@PathVariable int count, HttpSession session) {
@@ -35,7 +40,17 @@ public class RecommendationController {
 			userId = principal.getUserID();
 			List<Integer> foodIdList = svc.getLatestPreferredFoodItem(userId);
 			// TODO calculate required calories
-			int requiredCalories = 500;
+			SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+			String date = sdf.format(new Date());
+
+			int sumCalorieBurnt = calorieService.getSumCaloriesBurnt(userId, date);
+			int sumCalorieConsumed = calorieService.getSumCaloriesConsumed(userId, date);
+			int dailyCalories = calorieService.getTargetWeight(userId);
+			int netCalorie = sumCalorieConsumed - sumCalorieBurnt;
+			netCalorie = netCalorie > 0 ? netCalorie : 0;
+
+			int requiredCalories = (dailyCalories - netCalorie) / 3;
+
 			return svc.getRecommendationsForUser(foodIdList, requiredCalories, count);
 		}
 		return null;
