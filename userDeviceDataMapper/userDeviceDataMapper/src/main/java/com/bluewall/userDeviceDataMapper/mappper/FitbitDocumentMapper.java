b5a0e6473bf43564b3f62d3e28f0c8ff6558e313
Amit Dubey <amit.dubey2219@gmail.com>
2016-05-12 11:07:28 -0700

    fix date parsing exception in fitbit mapper

--- userDeviceDataMapper/userDeviceDataMapper/src/main/java/com/bluewall/userDeviceDataMapper/mappper/FitbitDocumentMapper.java
+++ userDeviceDataMapper/userDeviceDataMapper/src/main/java/com/bluewall/userDeviceDataMapper/mappper/FitbitDocumentMapper.java
@@ -5,6 +5,8 @@ import com.bluewall.userDeviceDataMapper.util.Constants;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.bson.Document;
 
@@ -15,6 +17,7 @@ import org.bson.Document;
 @Slf4j
 public class FitbitDocumentMapper implements ActivityLogMapper<Document> {
 
+    SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-dd");
     @Override
     public ActivityLog map(Document doc) {
         ActivityLog output = null;
@@ -24,7 +27,8 @@ public class FitbitDocumentMapper implements ActivityLogMapper<Document> {
                     .userID(getUserId(doc))
                     .loggedFrom(Constants.FITBIT_LOGGED_FROM)
                     .activityName(Constants.DEFAULT_ACTIVITY_NAME)
-                    .caloriesBurnt(getCaloriesBurnt(doc)).activityLogDate(getActivityLogDate(doc))
+                    .caloriesBurnt(getCaloriesBurnt(doc))
+                    .activityLogDate(getActivityLogDate(doc))
                     .build();
         } catch (Throwable t) {
             log.debug("Could not map {} to ActivityLog.\n{}", doc.toJson(), t);
@@ -32,8 +36,9 @@ public class FitbitDocumentMapper implements ActivityLogMapper<Document> {
         return output;
     }
 
-    private Date getActivityLogDate(Document doc) {
-		return (Date) doc.getDate(Constants.ACTIVITY_LOG_DATE_KEY);
+    private Date getActivityLogDate(Document doc) throws ParseException {
+        java.util.Date utilDate = formatter.parse(doc.getString(Constants.ACTIVITY_LOG_DATE_KEY));
+        return new java.sql.Date(utilDate.getTime());
 	}
 
 	private int getUserId(Document doc) {
